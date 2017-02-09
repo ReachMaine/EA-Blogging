020c6a566ebdc8e09b16c1b008dc1b7384a1f32e
Linda "zig" Speight <linda@reachmaine.com>
2017-02-09 16:12:32 -0500

    getting started.

---
+++ README.md
@@ -0,0 +1,2 @@
+# EA-Bloggin
+child template files for blogging on the EA multisite
---
+++ ea-blogging/functions.php
@@ -0,0 +1,6 @@
+<?php
+  require_once(get_stylesheet_directory().'/custom/branding.php');
+  add_action('after_setup_theme', 'ea_setup');
+  function ea_setup() {
+  }
+?>
