07b6fab96fda6767f3a769a7eff8bd57343ae78b
Linda "zig" Speight <linda@reachmaine.com>
2017-02-13 12:03:21 -0500

    working on template.

--- ea-blogging/functions.php
+++ ea-blogging/functions.php
@@ -2,5 +2,32 @@
   require_once(get_stylesheet_directory().'/custom/branding.php');
   add_action('after_setup_theme', 'ea_setup');
   function ea_setup() {
-  }
+    /* add the topnav menu block */
+    register_nav_menu('top_nav', 'Top Nav');
+
+    if ( function_exists('register_sidebar') ){
+
+      /* add top banner ad widget */
+     register_sidebar(array(
+      'name' => 'Top Banner Ad',
+      'id' => 'topbanner',
+      'description' => 'Widget for a targetted banner ad.',
+      'before_widget' => '<div class="prl-span-12"><div id="%1$s" class=" %2$s ad-container">',
+      'after_widget'  => '</div></div>'
+
+      ));
+
+      register_sidebar(array(
+        'name' => 'Biz Today Ads',
+        'id' => 'biztoday',
+        'description' => 'Widget for Business today.',
+        'before_widget' => '<div class="prl-span-12"><div id="%1$s" class=" %2$s biztoday-container">',
+        'after_widget'  => '</div></div>'
+
+      ));
+    }
+
+  } // end of ea_setup()
+
+
 ?>
---
+++ ea-blogging/single.php
@@ -0,0 +1,116 @@
+<?php  /* template mods:
+    13Feb16 zig - copy from EA
+ */
+?>
+<?php get_header();?>
+<div class="prl-container">
+    <div class="prl-grid prl-grid-divider">
+    	<?php /* if(isset($prl_data['banner_before_single_title']) && $prl_data['banner_before_single_title']!='') echo '<div id="single-top-ad" class="prl-span-12"> <div class="ads_top ad-container">'.stripslashes($prl_data['banner_before_single_title']).'</div></div>'; */ ?>
+    	<?php if (is_active_sidebar('topbanner')) {
+			dynamic_sidebar( 'topbanner' );
+    	} ?>
+        <section id="main" class="prl-span-9"> <!-- single -->
+        	<?php if (is_active_sidebar('breaking-news')) {
+    		echo '<div class="eai-breaking-news prl-span-12">';
+    		dynamic_sidebar( 'breaking-news' );
+    		echo '</div>';
+    	} else { /*  echo '<!-- no breaking news right now -->'; */ } ?>
+		   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		   <article id="post-<?php the_ID(); ?>" <?php post_class('article-single'); ?>>
+		   		<?php if ( has_post_thumbnail() ) { /* set meta image for google custom search */
+		   			$gimgatts = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'full-size');
+		   			echo '<meta itemprop="image" content="'.$gimgatts[0].'"></meta>';
+		   		} ?>
+					<?php if( has_post_thumbnail() && get_post_meta($post->ID, 'pl_post_thumb', true)!='Disable') : ?>
+						<div class="single-post-thumbnail">
+							<?php
+							/* the_post_thumbnail('ea_featuredimg');  */
+							/* the_post_thumbnail('');  */
+							/* post_thumb(get_the_ID(), 500); */
+							the_post_thumbnail('ea_featuredimg');
+							$image = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'full-size'); // For pinterest sharing
+							?>
+						</div>
+						<div class="single-post-thumbnail-caption space-bot">
+							<?php if (function_exists('cc_featured_image_caption') ) {
+								cc_featured_image_caption();
+							} ?>
+						</div>
+					<?php endif; ?>
+			   <h1><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+			   <hr class="prl-grid-divider">
+				<div class ="single-meta">
+			   		<?php if (in_category('Obituaries')) {
+						post_meta(true/*date*/, false/* author */, false /* comment */, false/*cat*/, false /* view */ ) ;
+			   		} else {
+			   			if (get_user_meta( $authordata->ID, 'ts_fab_user_hide', false )) {
+				   			post_meta(true/*date*/, false/* author */, false /* comment */, true /*cat*/, false /* view */ ) ;
+				   		} else {
+				   			post_meta(true, true, false, true, false);
+				   		} /* date, author, comments, cat, view, updated? */
+				   	} ?>
+			   		<?php ea_social_share();?>
+				</div>
+			   <hr class="prl-grid-divider">
+			   <div class="prl-grid">
+					<div class="prl-span-12 prl-span-flip">
+						<div class="prl-entry-content clearfix">
+
+						   <?php if($prl_data['show_excerpt']=='Enable') {?><strong><?php the_excerpt(); ?></strong><?php }?>
+						   <?php /* zig xout 23Aug16  if (in_category('Obituaries')) { the_content();  }
+						   		else {
+						   		$relpostpos = get_post_meta($post->ID, 'pl_related', true);
+						   		echo '<!-- '.$relpostpos.' zig -->';
+						   		embed_related_content($relpostpos);
+						   	} */ ?>
+						   <?php /* embed_related_content(); echo '<!-- content -->'.$content; */ ?>
+						   <?php /* zig 23Aug16 ++ */ the_content(); ?>
+
+						   <?php wp_link_pages(array('before' => __('Pages','presslayer').': ', 'next_or_number' => 'number')); ?>
+						   <?php edit_post_link(__('Edit','presslayer'),'<p>','</p>'); ?>
+
+						   <?php if(isset($prl_data['banner_after_single_content']) && $prl_data['banner_after_single_content']!='') echo '<div class="hide-mobile"><center class="ad-container ad-in-content">'.stripslashes($prl_data['banner_after_single_content']).'</center></div>';?>
+
+						</div>
+					</div>
+
+			   </div>
+
+		   </article>
+
+		   <?php endwhile; endif; ?>
+
+		   <?php if($prl_data['post_author']!='Disable'):?>
+		   <div id="article_author" class="prl-article-author clearfix">
+		   		<hr class="prl-grid-divider">
+		   		<span class="author-avatar"><?php echo get_avatar(get_the_author_meta('email'), '100'); ?></span>
+				<div class="author-info">
+					<h4><?php _e('About the author', 'presslayer'); ?>:  <?php the_author_posts_link(); ?></h4>
+					<p><?php the_author_meta("description"); ?></p>
+				</div>
+			</div>
+			<?php endif;?>
+		   <?php comments_template(); ?>
+
+			<?php
+			$single_id = $post->ID;
+			if(get_post_meta($post->ID, 'pl_related', true)=='default' or get_post_meta($post->ID, 'pl_related', true)==''){
+				$related = $prl_data['related_post'];
+			}else{
+				$related = get_post_meta($post->ID, 'pl_related', true);
+			}
+			/* zig - comment out related posts stuff
+			if($related!='Disable') get_template_part( 'related-post'); */
+			?>
+			<?php if (is_active_sidebar('biztoday')) { echo '<div class="biz-today horizontal">'; dynamic_sidebar('biztoday'); echo '</div>'; } ?>
+        </section>
+
+        <aside id="sidebar" class="prl-span-3">
+
+            <?php get_sidebar('single');?>
+        </aside>
+    </div>
+</div>
+
+
+<?php get_footer();?>
