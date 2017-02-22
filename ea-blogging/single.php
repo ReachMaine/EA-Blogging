0459227273183b88f591b5bd3b1ee2a3a63b130a
Linda Speight <linda@reachmaine.com>
2017-02-22 10:35:08 -0500

    move title to first in single post
    move image into content.

--- ea-blogging/single.php
+++ ea-blogging/single.php
@@ -1,5 +1,6 @@
 <?php  /* template mods:
-    13Feb16 zig - copy from EA
+    13Feb17 zig - copy'd from EA
+    22Feb17 zig - put title first, move thumbnail into content. cleanup obit stuff.
  */
 ?>
 <?php get_header();?>
@@ -10,45 +11,18 @@
 			dynamic_sidebar( 'topbanner' );
     	} ?>
         <section id="main" class="prl-span-9"> <!-- single -->
-        	<?php if (is_active_sidebar('breaking-news')) {
-    		echo '<div class="eai-breaking-news prl-span-12">';
-    		dynamic_sidebar( 'breaking-news' );
-    		echo '</div>';
-    	} else { /*  echo '<!-- no breaking news right now -->'; */ } ?>
 		   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		   <article id="post-<?php the_ID(); ?>" <?php post_class('article-single'); ?>>
-		   		<?php if ( has_post_thumbnail() ) { /* set meta image for google custom search */
-		   			$gimgatts = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'full-size');
-		   			echo '<meta itemprop="image" content="'.$gimgatts[0].'"></meta>';
-		   		} ?>
-					<?php if( has_post_thumbnail() && get_post_meta($post->ID, 'pl_post_thumb', true)!='Disable') : ?>
-						<div class="single-post-thumbnail">
-							<?php
-							/* the_post_thumbnail('ea_featuredimg');  */
-							/* the_post_thumbnail('');  */
-							/* post_thumb(get_the_ID(), 500); */
-							the_post_thumbnail('ea_featuredimg');
-							$image = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'full-size'); // For pinterest sharing
-							?>
-						</div>
-						<div class="single-post-thumbnail-caption space-bot">
-							<?php if (function_exists('cc_featured_image_caption') ) {
-								cc_featured_image_caption();
-							} ?>
-						</div>
-					<?php endif; ?>
 			   <h1><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 			   <hr class="prl-grid-divider">
 				<div class ="single-meta">
-			   		<?php if (in_category('Obituaries')) {
-						post_meta(true/*date*/, false/* author */, false /* comment */, false/*cat*/, false /* view */ ) ;
-			   		} else {
+			   		<?php
 			   			if (get_user_meta( $authordata->ID, 'ts_fab_user_hide', false )) {
 				   			post_meta(true/*date*/, false/* author */, false /* comment */, true /*cat*/, false /* view */ ) ;
 				   		} else {
 				   			post_meta(true, true, false, true, false);
 				   		} /* date, author, comments, cat, view, updated? */
-				   	} ?>
+				    ?>
 			   		<?php ea_social_share();?>
 				</div>
 			   <hr class="prl-grid-divider">
@@ -56,16 +30,31 @@
 					<div class="prl-span-12 prl-span-flip">
 						<div class="prl-entry-content clearfix">
 
-						   <?php if($prl_data['show_excerpt']=='Enable') {?><strong><?php the_excerpt(); ?></strong><?php }?>
-						   <?php /* zig xout 23Aug16  if (in_category('Obituaries')) { the_content();  }
-						   		else {
-						   		$relpostpos = get_post_meta($post->ID, 'pl_related', true);
-						   		echo '<!-- '.$relpostpos.' zig -->';
-						   		embed_related_content($relpostpos);
-						   	} */ ?>
-						   <?php /* embed_related_content(); echo '<!-- content -->'.$content; */ ?>
+                            <?php if ( has_post_thumbnail() ) { /* set meta image for google custom search */
+            		   			$gimgatts = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'full-size');
+            		   			echo '<meta itemprop="image" content="'.$gimgatts[0].'"></meta>';
+            		   		} ?>
+        					<?php if( has_post_thumbnail() && get_post_meta($post->ID, 'pl_post_thumb', true)!='Disable') { ?>
+        						<div class="single-post-thumbnail">
+        							<?php
+        							/* the_post_thumbnail('ea_featuredimg');  */
+        							/* the_post_thumbnail('');  */
+        							/* post_thumb(get_the_ID(), 500); */
+        							the_post_thumbnail('ea_featuredimg');
+        							$image = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'full-size'); // For pinterest sharing
+        							?>
+        						</div>
+    							<?php if (function_exists('cc_featured_image_caption') ) {
+                                    echo '<div class="single-post-thumbnail-caption space-bot">';
+    								cc_featured_image_caption();
+                                    echo '</div>';
+    							} ?>
+
+        					<?php } /* end if has_post_thumbnail */ ?>
+
+
+						   <?php/*  if($prl_data['show_excerpt']=='Enable') {?><strong><?php the_excerpt(); ?></strong><?php } */ ?>
 						   <?php /* zig 23Aug16 ++ */ the_content(); ?>
-                           <!-- here zig -->
                            <?php if (function_exists('ea_blog_nav')) { ea_blog_nav(); } ?>
 						   <?php wp_link_pages(array('before' => __('Pages','presslayer').': ', 'next_or_number' => 'number')); ?>
 						   <?php edit_post_link(__('Edit','presslayer'),'<p>','</p>'); ?>
